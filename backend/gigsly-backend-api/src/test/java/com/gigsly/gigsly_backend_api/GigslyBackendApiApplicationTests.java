2e8abbcad6bdecf0ac215c031be724fa9ec60ebd
AdityaSharma0410 <melee.zeno@gmail.com>
2025-11-20 09:38:16 +0530

    added backend api dependencies

---
+++ backend/gigsly-backend-api/src/test/java/com/gigsly/gigsly_backend_api/GigslyBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigsly.gigsly_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigslyBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
